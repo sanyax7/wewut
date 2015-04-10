9c2cf1b15ad09cc644ba0c5718e9713a98d8a9a4
Alexander Paukste <fatwreck@gmail.com>
2015-04-10 17:38:21 +0100

    Adding assertDateWithFormat

--- test/Assert.java
+++ test/Assert.java
@@ -3,6 +3,9 @@ package wewut.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Assert {
   public static void assertThrows(Class ex, Runnable runnable) {
     Exception exThrown = null;
@@ -19,5 +22,9 @@ public class Assert {
     else {
       assertEquals(ex, exThrown.getClass());
     }
+  }
+
+  public static void assertDateWithFormat(String expected, String format, Date dt) {
+    assertEquals(expected, new SimpleDateFormat(format).format(dt));
   }
 }
